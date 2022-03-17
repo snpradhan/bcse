14ff04fbe251690c8565cd849f368b949d45eb0f
Sachin <sachin.pradhan@northwestern.edu>
2022-03-17 07:03:55 -0500

    setting ALLOWED_HOSTS in settings

--- bcse_site/bcse_site/settings.py
+++ bcse_site/bcse_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5rzd8xmwjp_&3txyul)evn(v*p@g&2aa%ax9t=5n$%cas&l=s#'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
