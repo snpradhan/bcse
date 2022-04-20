f3f8d051a915c37e481a0d353aaab7ff15ab949a
Sachin <sachin.pradhan@northwestern.edu>
2022-04-20 13:05:39 -0500

    display reservation status on Equipment Types and Equipment table

--- bcse_site/bcse_app/templatetags/bcse_extras.py
+++ bcse_site/bcse_app/templatetags/bcse_extras.py
@@ -26,3 +26,47 @@ def get_page_start_index(paginator, page_number):
 @register.filter
 def get_page_end_index(paginator, page_number):
   return paginator.page(page_number).end_index()
+
+@register.filter
+def get_current_reservation_status(equipment):
+  if equipment.status == 'A':
+    reservations = models.Reservation.objects.all().filter(delivery_date__lte=datetime.datetime.now(), return_date__gte=datetime.datetime.now(), equipment__id=equipment.id)
+    if reservations.count() == 1:
+      return 'Checked Out'
+    else:
+      return 'Available'
+  else:
+    return 'Unavailable'
+
+@register.filter
+def get_available_equipment_count(equipment_type):
+  if equipment_type.status == 'A':
+    total_equipment = models.Equipment.objects.all().filter(equipment_type__id=equipment_type.id, status='A').count()
+    checked_out_equipment = models.Reservation.objects.all().filter(delivery_date__lte=datetime.datetime.now(),
+                                                           return_date__gte=datetime.datetime.now(),
+                                                           equipment__equipment_type__id=equipment_type.id,
+                                                           equipment__status='A'
+                                                           ).count()
+    return total_equipment - checked_out_equipment
+  else:
+    return 0
+
+@register.filter
+def get_unavailable_equipment_count(equipment_type):
+  if equipment_type.status == 'A':
+    unavailable_equipment = models.Equipment.objects.all().filter(equipment_type__id=equipment_type.id, status='I').count()
+    return unavailable_equipment
+  else:
+    return 0
+
+@register.filter
+def get_checked_out_equipment_count(equipment_type):
+  if equipment_type.status == 'A':
+    checked_out_equipment = models.Reservation.objects.all().filter(delivery_date__lte=datetime.datetime.now(),
+                                                           return_date__gte=datetime.datetime.now(),
+                                                           equipment__equipment_type__id=equipment_type.id,
+                                                           equipment__status='A'
+                                                           ).count()
+    return checked_out_equipment
+  else:
+    return 0
