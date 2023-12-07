89ddfd775e6e570397ae7c1dbf0b0b413b469406
Sachin <sachin.pradhan@northwestern.edu>
2023-12-07 12:05:03 -0600

    add conditional photo_release_url token replacement

--- bcse_site/bcse_app/models.py
+++ bcse_site/bcse_app/models.py
@@ -890,7 +890,10 @@ def replace_workshop_tokens(text, workshop, registration):
   replaced_text = replaced_text.replace('[workshop_meetup_url]', workshop.meetup_link or '')
   replaced_text = replaced_text.replace('[isbe_url]', workshop.registration_setting.isbe_link or '')
 
-  replaced_text = replaced_text.replace('[photo_release_url]', settings.PHOTO_RELEASE_URL or '')
+  photo_release_text = ''
+  if not registration.user.photo_release_complete:
+    photo_release_text = 'We do not have a photo release on file for you. Please click <a href="%s">here</a> to complete it before attending this event.' % settings.PHOTO_RELEASE_URL
+  replaced_text = replaced_text.replace('[photo_release_url]', photo_release_text or '')
 
   return replaced_text
 
